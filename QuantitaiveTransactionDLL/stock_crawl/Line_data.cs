ce03452d962f55a3572974c8567cc0f96ad195c0
lioliu <744596028@qq.com>
2017-03-05 19:41:01 +0800

    create the Line_data's member variable

--- QuantitaiveTransactionDLL/stock_crawl/Line_data.cs
+++ QuantitaiveTransactionDLL/stock_crawl/Line_data.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace stock_crawl
 {
+    class Line
+    {
+        public string time { set; get; }
+        public string price { set; get; }
+        public string volume { set; get; }
+    }
     class Line_data
     {
+        public string code { set; get; }
+        public string pre_close { set; get; }
+        public string date { set; get; }
+        public string time { set; get; }
+        public string total { set; get; }
+        public string begin { set; get; }
+        public string end { set; get; }
+        public Line[] line { set; get; }
     }
 }
